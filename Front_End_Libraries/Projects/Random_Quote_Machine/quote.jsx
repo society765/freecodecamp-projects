e441c0300aa4b257025a6fbbd7859fa2132bdd9f
Xiao Wang <society765@gmail.com>
2019-04-13 21:20:00 -0400

    beautify

--- Front_End_Libraries/Projects/Random_Quote_Machine/quote.jsx
+++ Front_End_Libraries/Projects/Random_Quote_Machine/quote.jsx
@@ -11,16 +11,26 @@ class Quote extends React.Component {
 
     render() {
         var tweethref = "https://twitter.com/intent/tweet?text=" + this.props.quotes.quote;
-        var wikipagehref = 'https://en.wikipedia.org/wiki/' + this.props.quotes.author; 
+        var wikipagehref = 'https://en.wikipedia.org/wiki/' + this.props.quotes.author;
+        var statusSpin = this.props.status ? (<i class="fas fa-spinner fa-sm fa-spin" />) : null;
+
         return (
-            <div>
-                <div id="author"><a href={wikipagehref} target='_blank'>{this.props.quotes.author}</a></div>
-                <div id="text">{this.props.quotes.quote}</div>
-                <div id="div-new-quote">
-                    <button id="new-quote" onClick={this.props.newQuote} className='btn btn-primary'>New Quote</button>
-                </div>
-                <div id="div-tweet-quote">
-                    <a id="tweet-quote" href={tweethref} target='_blank'>Tweet!</a>
+            <div className='w-100 h-100 container'>
+                <div class='row border-bottom p-2' id="author"><h2>
+                    <a href={wikipagehref} target='_blank'> {this.props.quotes.author}</a>
+                </h2></div>
+                <div class='row border-bottom p-2 overflow-auto lead' id="text">{this.props.quotes.quote}</div>
+                <div class='row align-self-end' id='buttons'>
+                    <div className="col"></div>
+                    <div className="align-self-center"> {statusSpin} {this.props.status} </div>
+                    <div id="div-new-quote" class='d-flex align-items-center mr-2'>
+                        <button id="new-quote" onClick={this.props.newQuote} className='btn btn-primary'>New Quote</button>
+                    </div>
+                    <div id="div-tweet-quote" class='d-flex align-items-center mr-2'>
+                        <a id="tweet-quote" href={tweethref} target='_blank' class='btn btn-primary'>
+                            <i class="fab fa-twitter"></i> Tweet
+                        </a>
+                    </div>
                 </div>
             </div>
         );
@@ -29,6 +39,7 @@ class Quote extends React.Component {
 
 const mapStateToProps = function (state) {
     return {
+        status: state.status,
         quotes: state.quotes
     };
 }
--- Front_End_Libraries/Projects/Random_Quote_Machine/rdx.js
+++ Front_End_Libraries/Projects/Random_Quote_Machine/rdx.js
@@ -1,32 +1,40 @@
-const STARTFETCH = 'STARTFETCH'; 
-const ENDFETCH = 'ENDFETCH'; 
+const STARTFETCH = 'STARTFETCH';
+const ENDFETCH = 'ENDFETCH';
 
 const defaultState = {
-    quotes: {}
+    quotes: {},
+    status: ''
 }
 
-const getQuote = function(func){ 
-    func(); 
-    return {type: STARTFETCH}; 
+const getQuote = function (func) {
+    func();
+    return { type: STARTFETCH };
+}
+const endFetch = function (author, quote) {
+    return { type: ENDFETCH, quote: quote, author: author };
 }
 
-const endFetch = function(author, quote){ 
-    return {type: ENDFETCH, quote: quote, author: author}; 
+const statusReducer = function (state = defaultState.status, action) {
+    switch (action.type) {
+        case STARTFETCH: return ' fetching... ';
+        case ENDFETCH: return '';
+        default: return state;
+    }
 }
-
 const quoteReducer = function (state = defaultState.quotes, action) {
     switch (action.type) {
         case STARTFETCH: return state;
-        case ENDFETCH: return {quote: action.quote, author: action.author}; 
+        case ENDFETCH: return { quote: action.quote, author: action.author };
         default: return state;
     }
 }
 
 var store = Redux.createStore(Redux.combineReducers({
+    status: statusReducer,
     quotes: quoteReducer
 }));
-store.subscribe(function(){
-    console.log('Redux store:', store.getState()); 
+store.subscribe(function () {
+    console.log('Redux store:', store.getState());
 })
 
-export {store, getQuote, endFetch}; 
+export { store, getQuote, endFetch }; 
